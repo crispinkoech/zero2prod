a412d4dc905b4ab2cd6e5691598565834f3559f0
Crispin Koech <crispinkoech.ck@gmail.com>
2023-09-30 17:39:49 +0300

    Chapter 3: Add subscribe endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,29 @@
 use std::net::TcpListener;
 
-use actix_web::{dev::Server, web, App, HttpResponse, HttpServer, Responder};
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use actix_web::{dev::Server, web, App, HttpResponse, HttpServer};
+use serde::Deserialize;
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let app_factory = || {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscribe", web::post().to(subscribe))
+    };
+
+    let server = HttpServer::new(app_factory).listen(listener)?.run();
     Ok(server)
+}
+
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+async fn subscribe(form: web::Form<SubscribeFormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+#[derive(Deserialize)]
+struct SubscribeFormData {
+    email: String,
+    name: String,
 }
